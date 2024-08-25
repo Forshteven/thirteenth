c41bf603353171eb96df7ea88438d7c5718cd0db
Forshteven <geocinds@mail.ru>
2024-08-26 06:45:40 +0900

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,7 +3,7 @@ list_2 = []
 for i in range(3, 21):
     for k in range(1, 20):
         for n in range(1, 20):
-            if i == k + n and i%(k+n) == 0:
+            if i == k + n and i%(k+n) == 0 and k != n:
                 list_1.append(i)
                 list_2.append(sorted([k, n]))
             else:
